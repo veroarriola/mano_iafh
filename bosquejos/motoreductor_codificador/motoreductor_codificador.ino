1ccce157482e624df84ec887c8b476f58fb05345
veroarriola <v.arriola@ciencias.unam.mx>
2026-09-07 10:48:51 -0600

    Corrigiendo uso de interrupciones-

--- bosquejos/motoreductor_codificador/motoreductor_codificador.ino
+++ bosquejos/motoreductor_codificador/motoreductor_codificador.ino
@@ -11,8 +11,8 @@
 // Verde: tierra
 // Amarillo: 3.3 a 5V
 
-const int A_CLK_PIN = 2;    // Azul
-const int B_DT_PIN = 3;     // Blanco
+const int A_CLK_PIN = 2;    // Azul - INT0
+const int B_DT_PIN = 3;     // Blanco - INT1
 const int IN1 = 8;
 const int IN2 = 9;
 const int ENA = 10;
@@ -23,10 +23,10 @@ const int NUM_VUELTAS = 1;
 const int TICKS_POR_VUELTA = 7;
 const int MAX_COUNT = NUM_VUELTAS * TICKS_POR_VUELTA;
 
-int counter;
-int currentStateCLK;
-int lastStateCLK;
-bool ccw;
+volatile int counter;
+volatile int currentStateCLK;
+volatile int lastStateCLK;
+volatile bool ccw;
 
 
 String currentDir = "";
@@ -51,51 +51,51 @@ void setup() {
   // estado inicial del CLK
   lastStateCLK = digitalRead(A_CLK_PIN);
 
-  attachInterrupt(0, updateEncoder, CHANGE);
-	attachInterrupt(1, updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(A_CLK_PIN), updateEncoder, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(B_DT_PIN), updateEncoder, CHANGE);
 }
 
 void loop() {
 }
 
 void updateEncoder(){
-	// leer el estado actual del CLK
-	currentStateCLK = digitalRead(A_CLK_PIN);
+  // leer el estado actual del CLK
+  currentStateCLK = digitalRead(A_CLK_PIN);
 
-  // Si el ultimo estado actual del CLK es distinto, entonces ocurrió un pulso 
+  // Si el último estado actual del CLK es distinto, entonces ocurrió un pulso
   // reacciona solo a un cambio de estado para evitar un conteo doble
 	if (currentStateCLK != lastStateCLK && currentStateCLK == 1){
 
-    // si el estado del DT es diferente que el estado del CLK 
-    // entonces el encoder está rotando en sentido antihorario CCW asi que //decrementa		
-    if (digitalRead(B_DT_PIN) != currentStateCLK) {
-			counter --;
-			currentDir ="CCW";
-		} else {
-      // Encoder está rotando en sentido horario CW así que incrementa
-			counter ++;
-			currentDir ="CW";
-		}
+      // si el estado del DT es diferente que el estado del CLK
+      // entonces el encoder está rotando en sentido antihorario CCW asi que //decrementa
+      if (digitalRead(B_DT_PIN) != currentStateCLK) {
+              counter --;
+              currentDir ="CCW";
+          } else {
+        // Encoder está rotando en sentido horario CW así que incrementa
+              counter ++;
+              currentDir ="CW";
+          }
 
-		printStatus();
+          printStatus();
 
-    if(counter >= MAX_COUNT) {
-      if (!ccw) {
-        stop();
-        moveCCW();
-      } else {
-        Serial.println("ERROR: no es cw");
-        //stop();
+      if(counter >= MAX_COUNT) {
+        if (!ccw) {
+          stop();
+          moveCCW();
+        } else {
+          Serial.println("ERROR: no es cw");
+          //stop();
+        }
+      } else if (counter <= -MAX_COUNT) {
+        if (ccw) {
+          stop();
+          moveCW();
+        } else {
+          Serial.println("ERROR: no es ccw");
+          //stop();
+        }
       }
-    } else if (counter <= -MAX_COUNT) {
-      if (ccw) {
-        stop();
-        moveCW();
-      } else {
-        Serial.println("ERROR: no es ccw");
-        //stop();
-      }
-    }
 	}
 
 	// guarda el ultimo estado del CLK 
